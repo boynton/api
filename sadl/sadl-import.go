bc4d39505d1fcdc5ea839b3d559b8ea7aabac3b7
Lee R. Boynton <boynton@users.noreply.github.com>
2023-02-05 09:39:44 -0800

    tweaked SADL import, cleaned up

--- sadl/sadl-import.go
+++ sadl/sadl-import.go
@@ -1,27 +1,26 @@
 /*
-   Copyright 2022 Lee R. Boynton
+Copyright 2022 Lee R. Boynton
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-       http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 package sadl
 
 import (
-	"fmt"
+	"path/filepath"
 
-	"github.com/boynton/smithy"
 	"github.com/boynton/sadl"
-	"github.com/boynton/data"
 	sadlsmithy "github.com/boynton/sadl/smithy"
+	"github.com/boynton/smithy"
 )
 
 func Import(path string) (*smithy.AST, error) {
@@ -29,10 +28,10 @@ func Import(path string) (*smithy.AST, error) {
 	if err != nil {
 		return nil, err
 	}
-	model.Namespace = "example"
-	fmt.Println("->", data.Pretty(model))
-	ast, err := sadlsmithy.FromSADL(model, model.Namespace)
-	fmt.Println("THIS:", data.Pretty(ast))
-	return ast, err
-	//	return sadlsmithy.FromSADL(model, model.Namespace)
+	if model.Namespace == "" {
+		file := filepath.Base(path)
+		ext := filepath.Ext(path)
+		model.Namespace = file[:len(file)-len(ext)]
+	}
+	return sadlsmithy.FromSADL(model, model.Namespace)
 }
